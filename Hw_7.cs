7977aa7b872066f4c2d8dca58e6d550b46cbd301
KiraHaus <k.paladich@gmail.com>
2022-04-28 19:57:52 +0200

    Update Hw_7.cs

--- Hw_7.cs
+++ Hw_7.cs
@@ -1,14 +1,14 @@
 using Hw_7;
 
-Worker Ivan = new Worker();
-//Worker Ivan = new Worker(1, "Ivan", 23, 178, 23.08.1994, "Moscow");
-//Здесь не работает
-
-Ivan.Birth = DateOnly.Parse("23.08.1994");
-Ivan.Name = "Ivan Ivanych";
-Ivan.Age = 23;
-Ivan.Place = "Moscow";
-Ivan.Id = 1;
-Ivan.Height = 178;
-
-Ivan.ShowInfo(1);
+Worker Ivan = new Worker()
+{
+    Name = "Ivan",
+    Id = 1,
+    Date = DateTime.Now,
+    Birth = new DateOnly(1994, 08, 23),
+    Age = 23,
+    Place = "Moscow"
+};
+Worker Ivan2 = new Worker(1, "Ivan", 23, 176, new DateOnly(1994, 08, 23), "Moscow");
+Console.WriteLine(Ivan2.ShowInfo());
+Console.WriteLine(Ivan.ShowInfo());
