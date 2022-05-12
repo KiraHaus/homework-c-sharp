d9ef1ba1c41f48f00738c563de5b5c00e252f2ef
KiraHaus <k.paladich@gmail.com>
2022-05-12 18:25:59 +0200

    Update Hw_7.cs

--- Hw_7.cs
+++ Hw_7.cs
@@ -1,13 +1,12 @@
-using Hw_7;
-
-
-string path = @"C:\Users\paladich\Desktop\Skillbox\C sharp\7\Hw 7\database.txt";
+string path = @"C:\Users\paladich\Desktop\Skillbox\C sharp\7\Hw 7\database.xml";
 
 Base database = new Base(path);
 
 database.Load();
 
-database.AddWorker(new Worker(database.Count + 1, "Паладич Кирилл Максимович", 27, 178, new DateTime(1994, 08, 23), "Москва"));
-database.AddWorker(new Worker(database.Count + 1, "Паладич Кирилл", 27, 178, new DateTime(1994, 08, 23), "Москва"));
+//database.AddWorker(new Worker(database.Count + 1, "Паладич Кирилл Максимович", 27, 178, new DateTime(1994, 08, 23), "Москва"));
 
 database.Print();
+database.WriteData();
+
+database.Sort();
