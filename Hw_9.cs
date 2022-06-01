fc08018d41fbd5c2ced7b5eaf09b1292465d74a3
KiraHaus <k.paladich@gmail.com>
2022-06-01 20:13:16 +0200

    Update Hw_9.cs

--- Hw_9.cs
+++ Hw_9.cs
@@ -4,8 +4,8 @@ using Telegram.Bot.Extensions.Polling;
 using Telegram.Bot.Types.ReplyMarkups;
 using Telegram.Bot.Types;
 
-
 ITelegramBotClient bot = new TelegramBotClient("token");
+string path = @"Great Vault";
 async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
 {
     Console.WriteLine(JsonConvert.SerializeObject(update));
@@ -18,17 +18,18 @@ async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, Cancel
             await botClient.SendTextMessageAsync(message.Chat, "Приветствую, мешок с мясом!");
             return;
         }
+        else if(System.IO.File.Exists(@$"{path}\{message.Text}"))
+        {
+            var id = message.From.Id;
+            LoadDoc(id, message.Text);
+        }
+        else if (message.Text == "/command")
+        {
+            await botClient.SendTextMessageAsync(message.Chat, "/start - Запуск бота\r\n/list - Список файлов\r\n");
+        }
         else if (message.Text == "/list")
         {
-            DirectoryInfo vault = new DirectoryInfo(@"C:\Users\paladich\Desktop\Skillbox\C sharp\9\Great Vault");
-            foreach (FileInfo file in vault.GetFiles())
-            {
-                var inlineSelect = new InlineKeyboardMarkup(new[]
-                            {
-                                new[] {InlineKeyboardButton.WithCallbackData(file.Name)}
-                            });
-                await botClient.SendTextMessageAsync(message.Chat, "Файл", replyMarkup: inlineSelect);
-            }
+            await FileList(botClient, message);
         }
         else if (message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
         {
@@ -58,33 +59,47 @@ async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, Cancel
         }
     }
 }
-
+///Отображение доументов хранилища
+async Task FileList (ITelegramBotClient botClient, Message? message)
+{
+    DirectoryInfo vault = new DirectoryInfo(path);
+    var list = new List<KeyboardButton>();
+    foreach (FileInfo file in vault.GetFiles())
+    {
+        list.Add(new KeyboardButton(file.Name));
+    }
+    var markup = new ReplyKeyboardMarkup(list);
+    await botClient.SendTextMessageAsync(message.Chat, "All files in the Great Vault", replyMarkup: markup);
+}
+///Скачивание документа
 async void Download (string id, string name)
 {
     var file = await bot.GetFileAsync(id);
-    FileStream stream = new FileStream($@"C:\Users\paladich\Desktop\Skillbox\C sharp\9\Great Vault\{name}", FileMode.Create);
+    FileStream stream = new FileStream($@"{path}\{name}", FileMode.Create);
     await bot.DownloadFileAsync(file.FilePath, stream);
     stream.Close();
     stream.Dispose();
 }
-
+///Скачивание фото
 async void DownloadPhoto(string id, string name)
 {
     var file = await bot.GetFileAsync(id);
-    FileStream stream = new FileStream($@"C:\Users\paladich\Desktop\Skillbox\C sharp\9\Great Vault\{name}.jpg", FileMode.Create);
+    FileStream stream = new FileStream($@"{path}\{name}.jpg", FileMode.Create);
     await bot.DownloadFileAsync(file.FilePath, stream);
     stream.Close();
     stream.Dispose();
 }
-
-async void LoadDoc(string name)
+///Загрузка документов из хранилища
+async void LoadDoc(long id, string name)
 {
-    FileStream stream = new FileStream($@"C:\Users\paladich\Desktop\Skillbox\C sharp\9\Great Vault\{name}", FileMode.Open);
-    await bot.SendDocumentAsync(name, stream);
-    stream.Close();
-    stream.Dispose();
+    Stream fs = System.IO.File.OpenRead($@"{path}\{name}");
+    var document = new Telegram.Bot.Types.InputFiles.InputOnlineFile(fs);
+    document.FileName = name;
+    await bot.SendDocumentAsync(id, document);
+    fs.Close();
+    fs.Dispose();
 }
-
+///Исключения
 static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
 {
     Console.WriteLine(JsonConvert.SerializeObject(exception));
